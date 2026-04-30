902d5db19e9f17bf8c03dcfe9780b8f546c97b25
Dachi <2483133145@qq.com>
2026-04-30 18:57:52 +0800

    fix: 添加 404.html 插件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle() {
+        const src = path.resolve(__dirname, 'dist/index.html')
+        const dest = path.resolve(__dirname, 'dist/404.html')
+        fs.copyFileSync(src, dest)
+      }
+    }
+  ],
   base: './',
 })
